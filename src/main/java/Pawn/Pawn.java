bb7ef951a3988aff78c7c61a142fef5fae13df92
reka <debreka@gmail.com>
2022-04-11 14:41:55 +0200

    crated constructor and getter

--- src/main/java/Pawn/Pawn.java
+++ src/main/java/Pawn/Pawn.java
@@ -1,2 +1,11 @@
-package Pawn;public class Pawn {
+package Pawn;
+public class Pawn {
+    String color;
+
+    public Pawn(String lightOrDark){
+        color = lightOrDark;
+    }
+    public String getColor(){
+        return this.color;
+    }
 }
