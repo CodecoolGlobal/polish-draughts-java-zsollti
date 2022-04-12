34daede48c05a9facc21f7aa33f7fc124592d21f
reka <debreka@gmail.com>
2022-04-12 11:21:47 +0200

    set and get the position of pawns

--- src/main/java/Pawn/Pawn.java
+++ src/main/java/Pawn/Pawn.java
@@ -1,11 +1,38 @@
 package Pawn;
-public class Pawn {
-    String color;
 
-    public Pawn(String whiteOrBlack){
+
+
+public class Pawn {
+    private String color;
+    private Coordinates position;
+
+    public Pawn(String whiteOrBlack, int x, int y){
         color = whiteOrBlack;
+        position = new Coordinates(x, y);
     }
     public String getColor(){
         return this.color;
     }
+
+public class Coordinates{
+        private int x;
+        private int y;
+        public Coordinates (int coordX, int coordY){
+            this.x = coordX;
+            this.y = coordY;
+        }
+        public int getX(){
+            return this.x;
+        }
+        public void setX(int coordX){
+            this.x = coordX;
+        }
+    public int getY(){
+        return this.y;
+    }
+    public void setY(int coordY){
+        this.y = coordY;
+    }
+}
+
 }
