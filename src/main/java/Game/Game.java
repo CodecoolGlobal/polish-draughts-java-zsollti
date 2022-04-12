078ead5a146f522cf63882fdfc050cace635dcbc
zsollti <zsol.pz@gmail.com>
2022-04-12 13:40:08 +0200

    input validation, input convertation

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -1,30 +1,43 @@
 package Game;
 
 import Board.Board;
+import Pawn.Pawn;
 import Player.Player;
+import static Player.Player.Color.*;
+import static Player.Player.Direction.*;
+
+import java.util.Scanner;
 
 public class Game {
 
     private Board board;
-    private final Player POne;
-    private final Player PTwo;
+    private Player actualPlayer;
     private boolean isGameOn;
 
     public Game(){
         this.board = new Board();
-        this.POne = new Player();
-        this.PTwo = new Player();
         this.isGameOn = false;
+
     }
     public void start(){
+        this.isGameOn = true;
+        Player POne = new Player(WHITE);
+        Player PTwo = new Player(BLACK);
+        while(isGameOn){
+            this.actualPlayer = POne;
+            this.printBoard(POne, PTwo);
+            int[] startPos = this.getStartingPosition();
+            int[] endPos = this.getEndingPosition();
+            this.tryToMakeMove(startPos, endPos);
 
+        }
     }
 
     public void playRound(){
 
     }
 
-    public void tryToMakeMove(){
+    public void tryToMakeMove(int[] startingPos, int[] endingPos){
 
     }
 
@@ -32,15 +45,73 @@ public class Game {
 
     }
 
-    private void setIsGameOn(){
-        this.isGameOn = !this.isGameOn;
+    private void printBoard(Player POne, Player PTwo){
+        System.out.flush();
+        POne.printName();
+        this.board.toString();
+        PTwo.printName();
     }
 
-    public void printBoard(){
-        // this.board.toString();
+    private boolean isIndexesValid(int[] indexes){
+        if (this.board.size() >= indexes[0] && this.board.size() >= indexes[1] && indexes[0] >= 0 && indexes[1] >= 0){
+            return true;
+        }
+        return false;
     }
 
-    private void getUserInput(){
+    private boolean isPawnAtIndex(int[] indexes){
+        Pawn pawn = board.getPawn(indexes[0], indexes[1]);
+        if(pawn != null){
+            if(pawn.getColor() == actualPlayer.getColor()){
+                return true;
+            }
+        }
+        return false;
+    }
 
+    private int[] convertCoordinateToIndexes(String input){
+        int[] indexes = new int[2];
+        indexes[0] = (int)input.charAt(0) - 97;
+        if(input.length() == 2){
+            indexes[1] = input.charAt(1) - 1;
+        }
+        else{
+            indexes[1] = Integer.parseInt(input.substring(1,2)) - 1;
+        }
+        return indexes;
+    }
+
+    private int[] getStartingPosition(){
+        boolean isInputValid = false;
+        int[] indexes = new int[2];
+        while(!isInputValid){
+            Scanner scanner = new Scanner(System.in);
+            String coordinate = scanner.nextLine();
+            coordinate = coordinate.toLowerCase();
+            indexes = convertCoordinateToIndexes(coordinate);
+            if(isIndexesValid(indexes)){
+                   if(isPawnAtIndex(indexes)){
+                       isInputValid = true;
+                   }
+            }
+        }
+        return indexes;
+    }
+
+    private int[] getEndingPosition(){
+        boolean isInputValid = false;
+        int[] indexes = new int[2];
+        while(!isInputValid){
+            Scanner scanner = new Scanner(System.in);
+            String coordinate = scanner.nextLine();
+            coordinate = coordinate.toLowerCase();
+            indexes = convertCoordinateToIndexes(coordinate);
+            if(isIndexesValid(indexes)){
+                if(board.getPawn(indexes[0], indexes[1]) == null){
+                    isInputValid = true;
+                }
+            }
+        }
+        return indexes;
     }
 }
--- src/main/java/Player/Player.java
+++ src/main/java/Player/Player.java
@@ -2,17 +2,42 @@ package Player;
 
 import java.util.Scanner;
 
-public class Player {
-    private final String name;
 
-    public Player(){
+public class Player {
+    public enum Color {
+        BLACK,
+        WHITE
+    }
+    public enum Direction {
+        UP,
+        DOWN
+    }
+    private final String name;
+    private final Color color;
+    private final Direction direction;
+    public Player(Color color){
         System.out.println("New player: ");
         Scanner scanner = new Scanner(System.in);
         this.name = scanner.nextLine();
+        this.color = color;
+        if(color == Color.WHITE){
+            this.direction = Direction.DOWN;
+        }
+        else{
+            this.direction = Direction.UP;
+        }
         System.out.flush();
     }
 
-    public String getName(){
-        return this.name;
+    public void printName(){
+        System.out.println("Player: " + this.name);
+    }
+
+    public Color getColor(){
+        return this.color;
+    }
+
+    public Direction getDirection(){
+        return direction;
     }
 }
