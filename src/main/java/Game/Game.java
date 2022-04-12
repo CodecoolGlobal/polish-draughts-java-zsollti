7e632b771a0ceac8f8fb6b7685ad0007247e101c
reka <debreka@gmail.com>
2022-04-12 15:08:41 +0200

    check for winner

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -41,7 +41,28 @@ public class Game {
 
     }
 
-    public void checkForWinner(){
+    public String checkForWinner(){
+        boolean hasBlack = false;
+        boolean hasWhite = false;
+
+        for(int i = 0; i < this.board.size(); i++){
+            for(int j = 0; j < this.board.size(); j ++){
+                if(this.board.getBoard()[i][j] != null ){
+                    if(this.board.getBoard()[i][j].getColor() == "black"){
+                        hasBlack = true;
+                    }else{
+                        hasWhite = true;
+                    }
+            }
+        }
+            if (!hasBlack) {
+                return "white";
+            }else if (!hasWhite){
+                return "black";
+            }else{
+                return "";
+            }
+
 
     }
 
